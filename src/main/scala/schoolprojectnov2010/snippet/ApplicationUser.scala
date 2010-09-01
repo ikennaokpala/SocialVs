0f0b94f1cfd83c56ce7cdbea595b7f892454f822
IKENNA OKPALA <ikennaokpala@IKENNA-OKPALAs-MacBook.local>
2010-09-01 04:22:19 +0100

    `basic commit

--- src/main/scala/schoolprojectnov2010/snippet/ApplicationUser.scala
+++ src/main/scala/schoolprojectnov2010/snippet/ApplicationUser.scala
@@ -2,14 +2,13 @@ package rootsocial.snippet
 
 import net.liftweb.http._
 
-import rootsocial.model.{TwitterUser}
+import rootsocial.model.TwitterUser
 
 object UserSession extends SessionVar[Option[TwitterUser]](None)
 
 class ApplicationUser {
-
   var user: TwitterUser =
-    UserSession.is.getOrElse(createUserForSession)
+  UserSession.is.getOrElse(createUserForSession)
 
   // hold instance of TwitterUser in Session
   def createUserForSession: TwitterUser = {
