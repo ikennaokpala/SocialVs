defd5c2bb63d7489f8588d5e299add9d6b98f48a
Ikenna Okpala <ikenna@ubscribe.(none)>
2010-10-25 07:23:10 +0100

    Added influencer by and of by of url from klout is still returning xhtml, rather than a list of influencers

--- src/main/scala/schoolprojectnov2010/model/ApplicationActor.scala
+++ src/main/scala/schoolprojectnov2010/model/ApplicationActor.scala
@@ -10,28 +10,28 @@ import net.liftweb.http.S
 
 
 trait ExtUserProps extends UserProps with Js {
-    val id = 'id ! num
-    val listed_count = 'listed_count ! num
-    val favourites_count = 'favourites_count ! num
-    val url = 'url ! str
-    val friends_count = 'friends_count ! num
-    val profile_image_url = 'profile_image_url ! str
-    val name = 'name ! str
-    val description = 'description ! str
-    val location = 'location ! str
-    val statuses_count = 'statuses_count ! num
+  val id = 'id ! num
+  val listed_count = 'listed_count ! num
+  val favourites_count = 'favourites_count ! num
+  val url = 'url ! str
+  val friends_count = 'friends_count ! num
+  val profile_image_url = 'profile_image_url ! str
+  val name = 'name ! str
+  val description = 'description ! str
+  val location = 'location ! str
+  val statuses_count = 'statuses_count ! num
 }
 object ExtUser extends ExtUserProps with Js
 
 object TwitterCredentials {
-    val consumerKey = "ZbjvoQtqD56WzVGsJYYqzw" // OAuth application key, top-secret
-    val consumerSecret = "plgoNIGZOic9keO4BEHZ3aHRdRC6N0JJMGJPOpg"
-    val consumer = Consumer(consumerKey, consumerSecret)
+  val consumerKey = "ZbjvoQtqD56WzVGsJYYqzw" // OAuth application key, top-secret
+  val consumerSecret = "plgoNIGZOic9keO4BEHZ3aHRdRC6N0JJMGJPOpg"
+  val consumer = Consumer(consumerKey, consumerSecret)
 }
 
 object KloutCredentials {
-    val key = "n6aahpgj7geqespvdvsbuk7u" // Klout Application key
-    val req = :/("api.klout.com") / "1"
+  val key = "n6aahpgj7geqespvdvsbuk7u" // Klout Application key
+  val req = :/("api.klout.com") / "1"
 }
 
 case object AuthURL
@@ -44,117 +44,134 @@ case class TwitterUserVO(id: BigDecimal, name: String, screenName: String,
                          description: String, text: String, statuses_count: BigDecimal,
                          friends_count: BigDecimal, followers_count: BigDecimal,
                          listed_count: BigDecimal, favourites_count: BigDecimal,
-                         /*url: String,*/ location: String, profile_image_url: String,
+                         location: String, profile_image_url: String,
                          score: BigDecimal, true_reach: BigDecimal,
                          amplification_score: BigDecimal, network_score: BigDecimal)
 
 class ApplicationActor extends LiftActor {
-    val http = new Http
-    var request_token: Option[Token] = None
-    var access_token: Option[Token] = None
-    val httpHost = S.hostAndPath
+  val http = new Http
+  var request_token: Option[Token] = None
+  var access_token: Option[Token] = None
+  val httpHost = S.hostAndPath
 
-    def messageHandler = { // Lift Actors message handler
-        case AuthURL =>
-            try {
-                val tok = http(Auth.request_token(TwitterCredentials.consumer, httpHost)) // request token for the application, as opposed to the user consider using Future this could throw a 503
+  def messageHandler = { // Lift Actors message handler
+    case AuthURL =>
+      try {
+        val tok = http(Auth.request_token(TwitterCredentials.consumer, httpHost)) // request token for the application, as opposed to the user consider using Future this could throw a 503
 
-                request_token = Some(tok)
-                val url = Auth.authenticate_url(tok).to_uri.toString // generate the url the user needs to go to, to grant us access authorize_url(tok).to_uri
-                reply(Some(url))
+        request_token = Some(tok)
+        val url = Auth.authenticate_url(tok).to_uri.toString // generate the url the user needs to go to, to grant us access authorize_url(tok).to_uri
+        reply(Some(url))
 
-            } catch {
-                case ex => reply((None, None))
-            }
+      } catch {
+        case ex => reply((None, None))
+      }
 
-        case OAuthResponse(vrfr) =>
+    case OAuthResponse(vrfr) =>
 
-            val accessToken = http(Auth.access_token(TwitterCredentials.consumer, // this  could throw a NoSuchElementException
-                request_token.get, vrfr))
+      val accessToken = http(Auth.access_token(TwitterCredentials.consumer, // this  could throw a NoSuchElementException
+        request_token.get, vrfr))
 
-            reply(Some(accessToken._3))
+      reply(Some(accessToken._3))
 
-        case Tweets(screenName) =>
+    case Tweets(screenName) =>
 
-            try {
-                val reqScore = KloutCredentials.req / "users" / "show.json" <<? Map("key" -> KloutCredentials.key, "users" -> screenName)
-                val reqInfluencer = KloutCredentials.req / "soi" / "influenced_by.json" <<? Map("key" -> KloutCredentials.key, "users" -> screenName)
-                val reqInfluencerOf = KloutCredentials.req / "soi" / "influenced_of.json" <<? Map("key" -> KloutCredentials.key, "users" -> screenName)
-                val twt = {
-                    val kjson = http(reqScore ># {'users ! list}) map {'score ! obj}
-                    val score = ('kscore ! num)(kjson(0))
-                    val true_reach = ('true_reach ! num)(kjson(0))
-                    val amplification_score = ('amplification_score ! num)(kjson(0))
-                    val network_score = ('network_score ! num)(kjson(0))
+      try {
+        val reqScore = KloutCredentials.req / "users" / "show.json" <<? Map("key" -> KloutCredentials.key, "users" -> screenName)
+        val reqInfluencedBy = KloutCredentials.req / "soi" / "influenced_by.json" <<? Map("key" -> KloutCredentials.key, "users" -> screenName)
+        val reqInfluencerOf = KloutCredentials.req / "soi" / "influenced_of.json" <<? Map("key" -> KloutCredentials.key, "users" -> screenName)
+        val twt = {
+          val kjson = http(reqScore ># {'users ! list}) map {'score ! obj}
+          val score = ('kscore ! num)(kjson(0))
+          val true_reach = ('true_reach ! num)(kjson(0))
+          val amplification_score = ('amplification_score ! num)(kjson(0))
+          val network_score = ('network_score ! num)(kjson(0))
 
-                    val twt1 = for{
+          val influencedBy = {
+            val twitter_screen_name = for{
+              influencerList <- http(reqInfluencedBy ># {'users ! list}) flatten {'influencers ! list}
+              screen_name = ('twitter_screen_name ! str)(influencerList)
+              kscore = ('kscore ! num)(influencerList)
+            } yield (screen_name, kscore)
+           twitter_screen_name
+          }
+          
+          val influencerOf = {
+            val twitter_screen_name = for{
+              influencerList <- http(reqInfluencerOf ># {'users ! list}) flatten {'influencers ! list}
+              screen_name = ('twitter_screen_name ! str)(influencerList)
+              kscore = ('kscore ! num)(influencerList)
+            } yield (screen_name, kscore)
+           twitter_screen_name
+          }
 
-                        twtJsonList <- http(Status(screenName).timeline)
-                        Status.user.screen_name(screen_name) = twtJsonList
-                        Status.text(text) = twtJsonList
-                        Status.user.followers_count(followers_count) = twtJsonList
-                        Status.user(user) = twtJsonList
-                        id = ExtUser.id(user)
-                        statuses_count = ExtUser.statuses_count(user)
-                        friends_count = ExtUser.friends_count(user)
-                        listed_count = ExtUser.listed_count(user)
-                        favourites_count = ExtUser.favourites_count(user)
-                       /* url = ExtUser.url(user)*/
-                        profile_image_url = ExtUser.profile_image_url(user)
-                        name = ExtUser.name(user)
-                        location = ExtUser.location(user)
-                        description = ExtUser.description(user)
+          val twt1 = for{
 
-                    } yield TwitterUserVO(id, name, screenName,
-                            description, text, statuses_count, friends_count,
-                            followers_count, listed_count, favourites_count,
-                            /*url,*/ location, profile_image_url, score, true_reach,
-                            amplification_score, network_score)
-//                 println("THE TWITTER INFORMATION FOR"+screenName+" from aplication IS : "+twt1)
-                    twt1
+            twtJsonList <- http(Status(screenName).timeline)
+            Status.user.screen_name(screen_name) = twtJsonList
+            Status.text(text) = twtJsonList
+            Status.user.followers_count(followers_count) = twtJsonList
+            Status.user(user) = twtJsonList
+            id = ExtUser.id(user)
+            statuses_count = ExtUser.statuses_count(user)
+            friends_count = ExtUser.friends_count(user)
+            listed_count = ExtUser.listed_count(user)
+            favourites_count = ExtUser.favourites_count(user)
+            profile_image_url = ExtUser.profile_image_url(user)
+            name = ExtUser.name(user)
+            location = ExtUser.location(user)
+            description = ExtUser.description(user)
+
+          } yield TwitterUserVO(id, name, screenName,
+              description, text, statuses_count, friends_count,
+              followers_count, listed_count, favourites_count,
+              location, profile_image_url, score, true_reach,
+              amplification_score, network_score)
+          twt1
 
 
-                }
-                //        println(twt.asInstanceOf[List[TwitterUserVO]])
-                reply(twt)
-            } catch {
+        }
 
-                case ex => println (ex); reply((None, None))
-            }
+        reply(twt)
+      } catch {
 
-        case Mentions(screenName) =>
+        case ex => println(ex); reply((None, screenName))
+      }
 
-            val req = Twitter.host
+    case Mentions(screenName) =>
 
-        case InfluencersSearch(searchInput) =>
+      val req = Twitter.host
 
-            try {
+    case InfluencersSearch(searchInput) =>
 
-                val searchresult = {
-                    val req = KloutCredentials.req / "topics" / "search.json" <<? Map("key" -> KloutCredentials.key, "topic" -> searchInput)
-                    val kuser = for{
-                        json <- http(req ># {
-                            'users ! list
+      try {
 
-                        })
-                        user_name = ('user_name ! str)(json)
-                        score = ('skore ! num)(json)
-                        picture = ('pic_url ! str)(json)
-                    } yield KloutUser(user_name, score, picture, searchInput)
-                    kuser
-                }
+        val searchresult = {
+          val req = KloutCredentials.req / "topics" / "search.json" <<? Map("key" -> KloutCredentials.key, "topic" -> searchInput)
+          val kuser = for{
+            json <- http(req ># {
+              'users ! list
 
-                reply(searchresult)
-            } catch {
-                case ex => reply((None, None))
-            }
+            })
+            user_name = ('user_name ! str)(json)
+            score = ('skore ! num)(json)
+            picture = ('pic_url ! str)(json)
+          } yield KloutUser(user_name, score, picture, searchInput)
+          kuser
+        }
 
-        case _ => println("unkown message")
+        reply(searchresult)
+      } catch {
+        case ex => reply((None, None))
+      }
 
-    }
+    case _ => println("unkown message")
+
+  }
 
 }
 
 
-
+//                 println("THE TWITTER INFORMATION FOR "+screenName+"  FROM APPLICATION IS : "+twt1)
+    
 
--- src/main/scala/schoolprojectnov2010/snippet/TProfile.scala
+++ src/main/scala/schoolprojectnov2010/snippet/TProfile.scala
@@ -9,75 +9,78 @@ import scala.xml.NodeSeq
 import schoolprojectnov2010.model.TwitterUserVO
 
 class TProfile extends ApplicationUser {
-    val screenName = S.param("screenName")
-    def render(xhtml: NodeSeq): NodeSeq =
-        user.authorized match {
-            case true => val tprofile = (for{
-                screenName <- S.param("screenName")
-            } yield userStream(screenName)) getOrElse noTweets
-            tprofile match {
-                case x :: rest => val TwtInfoList = tprofile.asInstanceOf[List[TwitterUserVO]]
-//                println("THE TWITTER INFORMATION FOR"+screenName+"  IS :"+TwtInfoList)
-                val TwtUserInfo = TwtInfoList(0)
-                val text = TwtInfoList map (n => <li>
-                    {n.text}
-                </li>)
-                bind("p", xhtml,
-                    "name" -> TwtUserInfo.name.toString,
-                    "description" -> TwtUserInfo.description,
-                    "screen_name" -> TwtUserInfo.screenName,
-                    "screen_name_anchor" -> <a>@
-                        {TwtUserInfo.screenName}
-                    </a>,
-                    /*"url" -> TwtUserInfo.url,*/
-                    "favourites_count" -> TwtUserInfo.favourites_count.toString,
-                    "listed_count" -> TwtUserInfo.listed_count.toString,
-                    "text" -> text,
-                    "location" -> TwtUserInfo.location.toString,
-                    "statuses_count" -> TwtUserInfo.statuses_count.toString,
-                    "followers_count" -> TwtUserInfo.followers_count.toString,
-                    "profile_picture" -> <img src={TwtUserInfo.profile_image_url.toString} width=' ' height=' '/>,
-                    "friends_count" -> TwtUserInfo.friends_count.toString,
-                    "score" -> round(TwtUserInfo.score.doubleValue).toString,
-                    "truereach" -> round(TwtUserInfo.true_reach.doubleValue).toString,
-                    "amplification" -> round(TwtUserInfo.amplification_score.doubleValue).toString,
-                    "network" -> round(TwtUserInfo.network_score.doubleValue).toString)
+  val screenName = S.param("screenName")
 
-                case _ =>  bind("p", xhtml,
-                    "name" -> "Not a valid Twitter account !!",
-                    "description" -> "",
-                    "screen_name" -> "",
-                    "screen_name_anchor" -> "",
-                    /*"url" -> "",*/
-                    "favourites_count" -> "",
-                    "listed_count" -> "",
-                    "text" -> "",
-                    "location" -> "",
-                    "statuses_count" -> "",
-                    "followers_count" -> "",
-                    "profile_picture" ->"",
-                    "friends_count" -> "",
-                    "score" -> "",
-                    "truereach" -> "",
-                    "amplification" -> "",
-                    "network" -> "")
-            }
-            case false => notAuthorised;
-        }
+  def render(xhtml: NodeSeq): NodeSeq =
+    user.authorized match {
+      case true => val tprofile = (for{
+        screenName <- S.param("screenName")
+      } yield userStream(screenName)) getOrElse noTweets
+      tprofile match {
+        case x :: rest => val TwtInfoList = tprofile.asInstanceOf[List[TwitterUserVO]]
+        //                println("THE TWITTER INFORMATION FOR"+screenName+"  IS :"+TwtInfoList)
+        val TwtUserInfo = TwtInfoList(0)
+        val text = TwtInfoList map (n => <li>
+          {n.text}
+        </li>)
+        bind("p", xhtml,
+          "error" -> "",
+          "name" -> TwtUserInfo.name.toString,
+          "description" -> TwtUserInfo.description,
+          "screen_name" -> TwtUserInfo.screenName,
+          "screen_name_anchor" -> <a>@
+            {TwtUserInfo.screenName}
+          </a>,
+          
+          "favourites_count" -> TwtUserInfo.favourites_count.toString,
+          "listed_count" -> TwtUserInfo.listed_count.toString,
+          "text" -> text,
+          "location" -> TwtUserInfo.location.toString,
+          "statuses_count" -> TwtUserInfo.statuses_count.toString,
+          "followers_count" -> TwtUserInfo.followers_count.toString,
+          "profile_picture" -> <img src={TwtUserInfo.profile_image_url.toString} width=' ' height=' '/>,
+          "friends_count" -> TwtUserInfo.friends_count.toString,
+          "score" -> round(TwtUserInfo.score.doubleValue).toString,
+          "truereach" -> round(TwtUserInfo.true_reach.doubleValue).toString,
+          "amplification" -> round(TwtUserInfo.amplification_score.doubleValue).toString,
+          "network" -> round(TwtUserInfo.network_score.doubleValue).toString)
 
-    def userStream(screen_name: String) = {
-        val userInfoList = user.tweetsForName(screen_name)
-        userInfoList
+        case _ => val tprofileTuple = tprofile.asInstanceOf[Tuple2[Option[String], String]]
+        bind("p", xhtml,
+          "error" -> (tprofileTuple._2 + " is not a valid twitter user !!"),
+          "name" -> "",
+          "description" -> "",
+          "screen_name" -> "",
+          "screen_name_anchor" -> "",
+          "favourites_count" -> "",
+          "listed_count" -> "",
+          "text" -> "",
+          "location" -> "",
+          "statuses_count" -> "",
+          "followers_count" -> "",
+          "profile_picture" -> "",
+          "friends_count" -> "",
+          "score" -> "",
+          "truereach" -> "",
+          "amplification" -> "",
+          "network" -> "")
+      }
+      case false => notAuthorised;
     }
 
-    def noTweets: NodeSeq = <div>
-        no tweets here
-    </div>
+  def userStream(screen_name: String) = {
+    val userInfoList = user.tweetsForName(screen_name)
+    userInfoList
+  }
 
-    def notAuthorised: NodeSeq = <center>
-        <h2>
-            You need to login to used this application
-        </h2>
-    </center>
+  def noTweets: NodeSeq = <div>
+    no tweets here
+  </div>
+
+  def notAuthorised: NodeSeq = <center>
+    <h2>
+      You need to login to used this application
+    </h2>
+  </center>
 
 }
